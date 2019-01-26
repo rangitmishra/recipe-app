abc29b34e70b081f001e8541b9dbb39889587391
Ranjeet Kumar Mishra <ranjeet.mishra@delhivery.com>
2019-01-26 19:06:13 +0530

    refactored using lombok.

--- src/main/java/com/ranjeetbaba/recipeapp/controllers/IndexController.java
+++ src/main/java/com/ranjeetbaba/recipeapp/controllers/IndexController.java
@@ -1,13 +1,16 @@
 package com.ranjeetbaba.recipeapp.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Slf4j
 @Controller
 public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage() {
+        log.debug("Inside IndexController-getIndexPage");
         return "index";
     }
 }
--- src/main/java/com/ranjeetbaba/recipeapp/domain/Ingredient.java
+++ src/main/java/com/ranjeetbaba/recipeapp/domain/Ingredient.java
@@ -1,10 +1,11 @@
 package com.ranjeetbaba.recipeapp.domain;
 
-import org.hibernate.annotations.Fetch;
+import lombok.*;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
 @Entity
 public class Ingredient {
     @Id
@@ -19,43 +20,4 @@ public class Ingredient {
     @OneToOne(fetch = FetchType.EAGER)
     private UnitOfMeasure unitOfMeasure;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public UnitOfMeasure getUnitOfMeasure() {
-        return unitOfMeasure;
-    }
-
-    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
-        this.unitOfMeasure = unitOfMeasure;
-    }
 }
